eb3c0e9b5bf2ed6d39f8af8bd4c7795225c52b5d
Oberlan Romao <oberlan@gmail.com>
2021-02-01 16:32:09 -0300

    Revisão ponteiro

--- 0 - Revisao/aula0.c
+++ 0 - Revisao/aula0.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define FALSE 0
-#define TRUE 1
-
-
 int main(int argc, char *argv[]) {
-    int x;
-    x = TRUE;
-    if (x) {
-        printf("OK\n");
-    }
+    int *x;
+    int y = 2;
+    x = &y;
+    printf("%p %p\n", x, &y);
+    printf("%d %d\n", *x, y);
+    *x = 3; // y = 3
+    printf("%d %d\n", *x, y);
+    y = 5; // *x = 5
+    printf("%d %d\n", *x, y);
     return 0;
 }

50cf1b87c92013704fdd0c2d0dde880ebe090f69
Oberlan Romao <oberlan@gmail.com>
2021-02-01 17:27:32 -0300

    Revisão alocação de memória

--- 0 - Revisao/aula0.c
+++ 0 - Revisao/aula0.c
@@ -5,28 +5,20 @@
 #define TAM 10
 
 int main(int argc, char *argv[]) {
-    
-    int t;
-    printf("Tamanho: ");
-    scanf("%d", &t);
-    int v[t];
-    v[t-1] = 13;
-    t++;
-    printf("%d\n", v[t-1]);
 
-    int *x = malloc(10*sizeof(int));
+    int *x = malloc(sizeof(int));
     if (x == NULL){
         printf("malloc deu erro!\n");
         return 1;
     }
+    x[0] = 13; //*x = 13 ==> *(x + 0) = 13
+    int *y = x;
 
-    int i = 1;
-    x[i] = 10; // x[0] = 10
-    x[1] = 11;
-    
-    printf("%d\n", x[i]); //*(i + x)
-    printf("%d\n", x[1]); //x[1]
+    printf("%p %p\n", x, y);
+    printf("%d\n", x[0]); //*(i + x)
+    printf("%d\n", y[0]);
 
-    free(x);
+    printf("%p\n", y);
+
     return 0;
 }

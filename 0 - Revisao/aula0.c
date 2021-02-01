4072223b453b8cc30c45b9624fd215abd1425d75
Oberlan Romao <oberlan@gmail.com>
2021-02-01 17:08:21 -0300

    Revisão malloc

--- 0 - Revisao/aula0.c
+++ 0 - Revisao/aula0.c
@@ -1,15 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// const int TAM = 10;
+#define TAM 10
+
 int main(int argc, char *argv[]) {
-    int *x;
-    int y = 2;
-    x = &y;
-    printf("%p %p\n", x, &y);
-    printf("%d %d\n", *x, y);
-    *x = 3; // y = 3
-    printf("%d %d\n", *x, y);
-    y = 5; // *x = 5
-    printf("%d %d\n", *x, y);
+    
+    int t;
+    printf("Tamanho: ");
+    scanf("%d", &t);
+    int v[t];
+    v[t-1] = 13;
+    t++;
+    printf("%d\n", v[t-1]);
+
+    int *x = malloc(10*sizeof(int));
+    if (x == NULL){
+        printf("malloc deu erro!\n");
+        return 1;
+    }
+
+    int i = 1;
+    x[i] = 10; // x[0] = 10
+    x[1] = 11;
+    
+    printf("%d\n", x[i]); //*(i + x)
+    printf("%d\n", x[1]); //x[1]
+
+    free(x);
     return 0;
 }
